cfdf5cdf7dc9e8a8a245cecc1f4395c792ea2a60
Whitney Hunter <whitney@aiontechnology.io>
2023-02-27 18:33:44 -0800

    Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Whitney Hunter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import './App.css';
+import {Header} from "./Components/Header";
+import {Navigation} from "./Components/Navigation";
+import {Main} from "./Components/Main";
+import {Footer} from "./Components/Footer";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Navigation/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
